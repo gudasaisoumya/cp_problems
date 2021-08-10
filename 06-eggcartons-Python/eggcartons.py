2c429f7dc3ecee3d6bb58a518270e42aa5874e40
gudasaisoumya <gudasaisoumya@users.noreply.github.com>
2021-08-10 17:03:55 +0530

    commit

--- 06-eggcartons-Python/eggcartons.py
+++ 06-eggcartons-Python/eggcartons.py
@@ -5,5 +5,17 @@
 
 
 def fun_eggcartons(eggs):
+	x=12
+	if (eggs == 0):
+		return 0
+	elif (1<=eggs<=x):
+		return 1
+	elif (12<=eggs<=x*2):
+		return 2
+	elif (24<=eggs<=x*3):
+		return 3
+	elif (36<=eggs<=x*4):
+		return 4
+	elif (48<=eggs<=x*5):
+		return 5
 	# your code goes here
-	return 1
