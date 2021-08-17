83f592ed4907c82626afd770d057fc5a2d160c42
gudasaisoumya <gudasaisoumya@users.noreply.github.com>
2021-08-17 21:12:00 +0530

    commit

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,7 +5,15 @@
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
+    result = [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                result[i][j] += m1[i][k] * m2[k][j]
+    for r in result:
+        print(r)
 
 
 
